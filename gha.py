361ae210bec6640e704e71a2711d39fe9e91daa7
pawelschulz <73879231+pawelschulz@users.noreply.github.com>
2025-04-29 13:58:19 +0200

    Update gha.py

--- gha.py
+++ gha.py
@@ -9,16 +9,29 @@ class GHA(object):
         
     def init(self, X):
         # TODO inicjuj macierz wag self.W o kształcie [n_components, ilość zmiennych ] 
-        
+        n_features = X.shape[1] # Liczba cech wejściowych
+        # Losowa inicjalizacja wag
+        self.W = np.random.randn(self.n_components, n_features) * 0.01 # Losowa inicjalizacja małymi wartościami
         return self
 
     def fit(self, X):
         # TODO algorytm uczenia GHA
-
+        self.init(X)
+        for epoch in range(self.n_epochs):
+            for x in X:
+                x = x.reshape(-1, 1)  # zmiana na wekor kolumnowy (784x1)
+                y = self.W @ x        # Obliczenie aktywacji neuronów: y = W * x
+                y = y.reshape(-1, 1)  # y na postać kolumnową
+                
+                #obliczenie zmiany wag według wzoru
+                delta_W = self.eta * ((y @ x.T) - np.tril(y @ y.T) @ self.W)
+                self.W += delta_W # aktualizacja wag
         return self
             
     def transform(self, X):
-        return (self.W @ X.T).T
+        # Rzutowanie danych X do przestrzeni głównych komponentów (nowe współrzędne w tej przestrzeni)
+        return (self.W @ X.T).T # X.T: [cechy x próbki] → wynik transponujemy do [próbki x komponenty]
 
     def inverse_transform(self, Y):
-        return Y @ self.W
+        # Odtworzenie oryginalnych danych na podstawie współrzędnych Y w przestrzeni komponentów
+        return Y @ self.W # Mnożenie współrzędnych przez macierz wag daje rekonstrukcję
